d719809b7e97159f2fe7e5b06464f380c3b04817
delogen74 <140206907+delogen74@users.noreply.github.com>
2024-08-05 23:54:30 +0500

    Update admin.py

--- fpages/admin.py
+++ fpages/admin.py
@@ -4,7 +4,6 @@ from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
 
 
-# Define a new FlatPageAdmin
 class FlatPageAdmin(FlatPageAdmin):
     fieldsets = (
         (None, {'fields': ('url', 'title', 'content', 'sites')}),
@@ -19,6 +18,5 @@ class FlatPageAdmin(FlatPageAdmin):
     )
 
 
-# Re-register FlatPageAdmin
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
